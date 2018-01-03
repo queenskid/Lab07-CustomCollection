a21c88d5d055b870a95e49b21f6792691d3f0eca
QueensKid <andres.ibarra0716@gmail.com>
2018-01-02 22:49:05 -0800

    not all data was sent to branch. had to push commits again.

---
+++ CollectionList/CollectionList/Inventory.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollectionList
+{
+    class Inventory<T> : IEnumerable<T>
+    {
+        T[] items = new T[10];
+        int count = 0;
+
+        public void AddJersey(T item)
+        {
+            if (count == (items.Length / 2))
+            {
+                T[] newArray = new T[items.Length * 2];
+
+                for (int i = 0; i < items.Length; i++)
+                {
+                    newArray[i] = items[i];
+                }
+
+                items = newArray;
+
+            }
+            items[count] = item;
+
+
+            count++;
+        }
+
+        public void RemoveJersey(T item)
+        {
+            int j = 0;
+            T[] newArray = new T[items.Length / 2];
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (!item.Equals(items[j]))
+                {
+                    if (j > count) return;
+                    newArray[i] = items[j];
+                    j++;
+                }
+                else
+                {
+                    j++;
+                }
+            }
+
+            public IEnumerator<T> GetEnumerator()
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    yield return items[i];
+                }
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+                {
+                return GetEnumerator();
+            }
+        }
+
+        internal object Remove(int input)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Add(object p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CollectionList/CollectionList/Products.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CollectionList
+{
+    class Product
+    {
+        public string ProductName { get; set; }
+        public JerseyID Category { get; set; }
+    }
+
+    enum JerseyID : int
+    {
+        JerseyNFL = 1,
+        JerseyNBA,
+        JerseyMLS,
+        JerseyMLB,
+        JerserNHL
+    }
+}
+
